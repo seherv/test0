3e319cf0ead51fb32dfe72eb315fbd6a9c78dd85
seherv <test-email@test.com>
2024-12-06 19:31:44 +0100

    Add other test cases

--- tests/test_example.py
+++ tests/test_example.py
@@ -26,3 +26,45 @@ def test_no_words():
 
     assert raw_word_count(example_input) == {}
 
+
+
+def test_one_long_word():
+    example_input = ('a' * 100).split()
+    assert raw_word_count(example_input) == {
+        'a' * 100: 1
+    }
+
+
+def test_uppercase():
+    example_input = """
+    HELLO WORLD!
+    THIS IS A SAMPLE TEXT FILE
+    HELLO PYTHON WORLD
+    """.split()
+
+    assert raw_word_count(example_input) == {
+        'hello': 2,
+        'world': 2,
+        'this': 1,
+        'is': 1,
+        'a': 1,
+        'sample': 1,
+        'text': 1,
+        'file': 1,
+        'python': 1
+    }
+
+
+def test_special_chracters():
+    example_input = '''
+    Hello!!!! Wor#$%#$%ld
+    AAAAA H#ello
+    '''.split()
+    assert raw_word_count(example_input) == {
+        'hello': 2,
+        'world': 1,
+        'aaaaa': 1,
+    }
+
+def test_file_word_count():
+    pass
